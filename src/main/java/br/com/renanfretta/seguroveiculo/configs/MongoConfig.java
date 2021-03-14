0cbb0eaeee66fd94230f1e1f787b9b587180a9fa
renanfretta <renanfretta@vhintranet.com.br>
2021-03-14 13:05:34 -0300

    Ajuste para colocar as configurações no arquivo de configuração

--- src/main/java/br/com/renanfretta/seguroveiculo/configs/MongoConfig.java
+++ src/main/java/br/com/renanfretta/seguroveiculo/configs/MongoConfig.java
@@ -1,5 +1,6 @@
 package br.com.renanfretta.seguroveiculo.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
@@ -13,6 +14,21 @@ import com.mongodb.client.MongoClients;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+
+	@Value("${spring.data.mongodb.host}")
+	private String host;
+
+	@Value("${spring.data.mongodb.port}")
+	private String port;
+
+	@Value("${spring.data.mongodb.username}")
+	private String username;
+
+	@Value("${spring.data.mongodb.password}")
+	private String password;
+
+	@Value("${spring.data.mongodb.database}")
+	private String database;
 
 	@Bean
 	public ValidatingMongoEventListener validatingMongoEventListener() {
@@ -31,14 +47,12 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
 	@Override
 	protected String getDatabaseName() {
-		// FIXME: Remover esta configuração estática
-		return "seguro-veiculo";
+		return database;
 	}
 
 	@Override
 	public MongoClient mongoClient() {
-		// FIXME: Remover esta configuração estática
-		final ConnectionString connectionString = new ConnectionString("mongodb://renanfretta:Ren%40nFretta1986@127.0.0.1:27017/seguro-veiculo?authSource=admin");
+		final ConnectionString connectionString = new ConnectionString("mongodb://" + username + ":" + password + "@" + host + ":" + port + "/" + database + "?authSource=admin");
 		final MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
 		return MongoClients.create(mongoClientSettings);
 	}
